bbfe51bd08712cdbfe7be23080944fff956e2614
fozodavid <fozo.david@gmail.com>
2021-02-20 22:57:21 +0100

    Daily Practice

--- 1basicDeclarations/150_read_and_evaluate.c
+++ 1basicDeclarations/150_read_and_evaluate.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 
 #define LEN 20
 
+struct symbol {
+  char data;
+  struct symbol *next;
+  struct symbol *prev;
+};
+
 int main() {
+  struct symbol *root = NULL;
+  struct symbol *head = NULL;
+  struct symbol *current = NULL;
+
   char exp[LEN];
   printf("Input an exp using +, -, *, / operators:\n");
   scanf("%s", exp);
 
-  int i = 0;
-  char signs[10];
+  current = malloc(sizeof(struct symbol));
+  head = current;
+  root = current;
   int s = 0;
-  while(exp[i] != '\0') {
-
-    if (exp[i] == '+' || exp[i] == '-') {
-      signs[s] = exp[i];
-      s++;
-    }
-    i++;
+  while(exp[s] != '\0') {
+    /* strcpy(sign, exp[s]); */
+    current -> data = exp[s];
+    current -> next = malloc(sizeof(struct symbol));
+    current = current -> next;
+    current -> prev = head;
+    head = current;
+    s++;
   }
 
-  printf("%s", signs);
+  current = root;
+  while (current -> next != 0) {
+    printf("%c", current -> data);
+    current = current -> next;
+  }
+
+  printf(" ");
+
+  current = head;
+  while (current -> prev != 0) {
+    current = current -> prev;
+    printf("%c", current -> data);
+  }
 
   return 0;
 }
