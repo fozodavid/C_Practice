7425634c3d9b1e84b45ad676997d62743bad075f
fozodavid <fozo.david@gmail.com>
2021-03-01 20:54:57 +0100

    Daily Practice

--- 1basicDeclarations/150_read_and_evaluate.c
+++ 1basicDeclarations/150_read_and_evaluate.c
@@ -1,23 +1,85 @@
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
-
 #define LEN 20
 
-int recurse() {
-  return 1;
-}
-
+/* const char OPERATORS[] = { '*', '/', '+', '-' }; */
 struct symbol {
   char data;
   struct symbol *next;
   struct symbol *prev;
 };
 
+
+int apply_operator(const struct symbol * head) {
+  head -> next -> prev = NULL;
+  head -> prev -> next = NULL;
+
+  switch(head -> data) {
+    /* case '*': */
+      /* return evaluate(head -> prev, 1) * evaluate(head -> next, 0); */
+    /* case '/': */
+      /* return evaluate(head -> prev, 1) / evaluate(head -> next, 0); */
+    case '+':
+      return evaluate(head -> prev, 1) + evaluate(head -> next, 0);
+    /* case '-': */
+      /* return evaluate(head -> prev, 1) - evaluate(head -> next, 0); */
+    default:
+      printf("Error\n");
+  }
+}
+
+
+int evaluate(const struct symbol *start, int left) {
+
+  if (start -> next == NULL && start -> prev == NULL) {
+    /* int result; */
+    /* sprintf(result, "%d", start -> data); */
+    return 1;
+  }
+
+  struct symbol * head = start;
+  if(!left) {
+    while(head -> next) {
+      if(head -> data == '+') {
+        return apply_operator(head);
+      }
+      head = head -> next;
+    }
+  } else {
+    while(head -> prev) {
+      if(head -> data == '+') {
+        return apply_operator(head);
+      }
+      head = head -> prev;
+    }
+  }
+
+  /* for (int i = 0; i < 4; i++) { */
+    /* struct symbol * head = start; */
+    /* if (!left) { */
+      /* while (head -> next) { */
+        /* if (head -> data == OPERATORS[i]) { */
+          /* return apply_operator(head); */
+        /* } */
+        /* head = head -> next; */
+      /* } */
+    /* } else { */
+      /* while (head -> prev) { */
+        /* if (head -> data == OPERATORS[i]) { */
+          /* return apply_operator(head); */
+        /* } */
+        /* head = head -> prev; */
+      /* } */
+    /* } */
+  /* } */
+
+}
+
 int main() {
   struct symbol *root = NULL;
   struct symbol *head = NULL;
-  struct symbol *current = NULL;
+  struct symbol *current = NULL; // clean this up
 
   char exp[LEN];
   printf("Input an exp using +, -, *, / operators:\n");
@@ -38,18 +100,14 @@ int main() {
   }
 
   current = root;
-  while (current -> next != 0) {
-    printf("%c", current -> data);
+  while(current) {
+    printf("This is a node: ");
+
+    printf("%p: %p %c %p\n", current, current -> prev, current -> data, current -> next);
     current = current -> next;
   }
 
-  printf(" ");
-
-  current = head;
-  while (current -> prev != 0) {
-    current = current -> prev;
-    printf("%c", current -> data);
-  }
+  /* printf("%d", evaluate(root, 0)); */
 
   return 0;
 }
