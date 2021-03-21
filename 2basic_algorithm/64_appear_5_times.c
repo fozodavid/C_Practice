483d66c6c56b9bc4f3bd2294f9f957dc1d7c5f1f
David Fozo <fozodavid@github.com>
2021-03-21 21:03:10 +0100

    Daily Practice

--- 2basic_algorithm/64_appear_5_times.c
+++ 2basic_algorithm/64_appear_5_times.c
@@ -5,23 +5,16 @@ int test(int arr[]) {
   int appear_5_times = 0;
   int v5_adjacent = 0;
   
-  int i = 0, k = 0;
+  int i = 0;
   int count = 0;
-  int selected = 0;
   while (i < LEN) {
-    selected = arr[i];
-    k = i;
-    count = 1;
-    while (k < LEN && !appear_5_times) {
-      if (arr[k] == selected) {
-        count++;
-        if (count == 5) appear_5_times = 1;
-        break;
-      } 
-      k++;
-    }
+    if (arr[i] == 5) {
+      count++;
+    } 
     i++;
   }
+
+  if (count >= 5) appear_5_times = 1;
 
   i = 0;
   while (i < LEN - 1) {
@@ -32,18 +25,18 @@ int test(int arr[]) {
     i++;
   }
 
-  printf("%d %d\n", appear_5_times, v5_adjacent);
   return appear_5_times && !v5_adjacent;
-
 }
 
 int main() {
   int arr1[LEN] = {1,2,3,4,5,6,7,8,9,10};
-  int arr2[LEN] = {1,1,1,1,1,6,7,8,9,10};
+  int arr2[LEN] = {5,1,5,1,5,1,5,1,5,10};
   int arr3[LEN] = {1,1,1,1,1,6,5,5,9,10};
+  int arr4[LEN] = {5,5,5,1,5,1,5,1,5,10};
 
   printf("%d\n", test(arr1));
   printf("%d\n", test(arr2));
   printf("%d\n", test(arr3));
+  printf("%d\n", test(arr4));
   return 0;
 }
---
+++ 2basic_algorithm/65_every_5_next_to_5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define LEN 15
+
+int test(arr[]) {
+
+}
+
+int main () {
+  int arr1[LEN] = {};
+  int arr2[LEN] = {};
+  int arr3[LEN] = {};
+  int arr4[LEN] = {};
+
+  printf("%d\n", test(arr1));
+  printf("%d\n", test(arr2));
+  printf("%d\n", test(arr3));
+  printf("%d\n", test(arr4));
+}
