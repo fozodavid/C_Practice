386c3dd022a886e861478b1d079edcb26871a54f
David Fozo <fozodavid@github.com>
2021-03-22 19:37:37 +0100

    Daily Practice

--- 2basic_algorithm/67_3_increasing.c
+++ 2basic_algorithm/67_3_increasing.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #define LEN 5
 
-int test(arr[]) {
-
+int test(int arr[]) {
+  int i = 0, k = 0;
+  int increasing3 = 0;
+  while (i < LEN - 2) {
+    if ((arr[i] == arr[i+1]-1) && (arr[i+1] == arr[i+2]-1)) {
+      increasing3 = 1;
+      break;
+    }
+    i++;
+  }
+  return increasing3;
 }
 
 int main() {
-  int arr1[LEN] = {};
-  int arr2[LEN] = {};
-  int arr3[LEN] = {};
+  int arr1[LEN] = {5,5,5,5,5};
+  int arr2[LEN] = {5,4,1,2,3};
+  int arr3[LEN] = {1,1,1,2,2};
+
+  printf("%d\n", test(arr1));
+  printf("%d\n", test(arr2));
+  printf("%d\n", test(arr3));
+
   return 0;
 }
