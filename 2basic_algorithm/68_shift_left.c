08356ed5a69fdaab4383a236697f445e0c6d07b2
David Fozo <fozodavid@github.com>
2021-03-25 21:35:15 +0100

    Daily Practice

--- 2basic_algorithm/68_shift_left.c
+++ 2basic_algorithm/68_shift_left.c
@@ -3,30 +3,25 @@
 #define LEN 5
 
 void test(int arr[], int * ptr) {
-  ptr = malloc(sizeof(int) * LEN);
-
-  int i = 1;
-  while (i < LEN) {
-    ptr[i - 1] = arr[i];
-    i++;
-  }
+  ptr = arr;
 }
 
 int main() {
-  int arr1[LEN] = {1,2,3,4,5};
-  int arr2[LEN] = {1,2,3,4,5};
-  int arr3[LEN] = {1,2,3,4,5};
-  int * ptr = NULL;
 
-  test(arr1, ptr);
-  test(arr2, ptr);
-  test(arr3, ptr);
+  int arr[LEN] = {0,1,2,3,4};
+  int * ptr = NULL;
+  ptr = malloc(sizeof(int) * LEN);
+
+  test(arr, ptr);
 
   int i = 0;
   while (i < LEN) {
-    printf("%d\n", ptr[i]);
+    printf("%d\n", *ptr);
+    ptr++;
     i++;
   }
   free(ptr);
+
   return 0;
 }
+
