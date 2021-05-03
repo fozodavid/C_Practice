e7b138dd20212d4b6549d59e603fe60819654c44
fozodavid <fozo.david@gmail.com>
2021-05-03 23:23:23 +0200

    Daily Practice

--- 6ForLoop/29_armstrong_number.c
+++ 6ForLoop/29_armstrong_number.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
+#include<math.h>
 
 int main () {
+  int number = 153;
+  int digits[10];
 
+  printf("Test Data: ");
+  scanf("%d", &number);
+
+  int i;
+  for (i = 0; i < 10; i++) {
+    digits[i] = (number % (int)(pow(10, (i + 1)))) / pow(10, i);
+  }
+
+  int sum = 0;
+  int power = log10((double)number) + 1;
+
+  for (i = 0; i < 10; i++) {
+    sum += (int)pow(digits[i], power);
+  }
+
+  if (sum == number) {
+    printf("This is an armstrong number");
+  } else {
+    printf("This is NOT an armstrong number");
+  }
 }
