66c86fc350e884587a8adba1a47c516d958506b0
David Fozo <fozo.david@gmail.com>
2021-05-20 22:00:17 +0200

    Daily Practice

--- 6ForLoop/50_decimal_to_octal.c
+++ 6ForLoop/50_decimal_to_octal.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
+  int input;
+  printf("Input: ");
+  scanf("%d", &input);
+
+  int digit = 0;
+  for (int i = 10; i >= 0; i--) {
+    digit = input / (int) pow(8.0, (double)i);
+    input = input - digit * pow(8.0, (double)i);
+    printf("%d", digit);
+  }
+  printf("\n");
+
   return 0;
 }
